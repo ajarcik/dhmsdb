50502b0ea0e201cddd27c9afc6d8ce2312b6e6b3
Moffatt, Moffatt <jamoffatt.411@gmail.com>
2024-09-06 08:36:57 -0400

    Uploading 9/6/2024
    Uploading some additional functions to the G-Spread File

--- GSpreadFunctions.py
+++ GSpreadFunctions.py
@@ -14,7 +14,9 @@ The purpose of this CreateDatabaseSpreadsheet function is create a Google spread
 @GoogleEmail - a Gmail account you have access to and the account the file will be shared with 
 '''
 
-
+'''
+Needs to:
+'''
 def CreateDatabaseSpreadsheet(nameofspreadsheet,ArrayOfDates,gc,GoogleEmail):
   sh = gc.create(nameofspreadsheet)
   time.sleep(2)
@@ -57,7 +59,7 @@ def add_data(firstname, lastname, email,gc,nameofspreadsheet,tabname):
   print("Done with adding data!!!")
 
 
-'''
+
 def movedata(LengthOfHolderList,gc,nameofspreadsheet,tabname,targetname):
   sh = gc.open(nameofspreadsheet)
   worksheet = sh.worksheet(tabname)
@@ -71,102 +73,80 @@ def movedata(LengthOfHolderList,gc,nameofspreadsheet,tabname,targetname):
   CellRange = 'A1:'+'D'+str(LengthOfHolderList)
   worksheet.update(HolderList,'A1:D2')
   print("Done!!")
-  '''
+  
+def get_key_by_value(dictionary, value):
+  for key, val in dictionary.items():
+      if val == value:
+          return key
 
+  return None  
 
-#Once all the volunteers events are run (go in and get the rows where number of events  =1, 2, 3, etc, etc)
-def formatcellsperuser(nameofspreadsheet,tabname,gc,checkedname):
-  sh = gc.open(nameofspreadsheet)
-  currentworksheet = sh.get_worksheet(tabname)
-  list_of_lists = currentworksheet.get_all_values()
-  element_to_check = checkedname
-  #required_count = 4
-  count = sum(sublist.count(element_to_check) for sublist in list_of_lists)
-  if count == 3:
-    print("GREEN")
-    cell_list = worksheet.findall("Joseph Moffatt")
-    worksheet.format(cell_list, {
-        "backgroundColor": {
-            "red": 0.0,
-            "green":1.0,
-            "blue": 0.0
-        },
-        "fontSize": 12,
-        "bold": True
-    })
-  elif count == 2:
-    print("NO GREEN")
-    cell_list = worksheet.findall("Joseph Moffatt")
-    worksheet.format(cell_list, {
-        "backgroundColor": {
-            "red": 0.0,
-            "green":0.0,
-            "blue": 1.0
-        },
-        "fontSize": 12,
-        "bold": True
-    })
-  elif count == 1:
-    print("RED")
-    cell_list = worksheet.findall("Joseph Moffatt")
-    worksheet.format(cell_list, {
-        "backgroundColor": {
-            "red": 1.0,
-            "green":0.0,
-            "blue": 0.0
-        },
-        "fontSize": 12,
-        "bold": True
-    })
-  else:
-    print("NOPE")
- 
-  print("All done!")
+def formatallcells(nameofspreadsheet,tabname,gc): #Concept
+    alphabet_mapping = {chr(i): i - 64 for i in range(65,91)}
+    sh = gc.open(nameofspreadsheet)
+    currentworksheet = sh.worksheet(tabname)
+    list_of_lists = currentworksheet.get_all_values()
+    A = get_key_by_value(alphabet_mapping,1)
+    O = get_key_by_value(alphabet_mapping,15)
+    for i in list_of_lists:
+        #print(i)
+        count_y = i.count('y')
+        #print(count_y)
+        cell = currentworksheet.find(i[1])
+    #print("Cell coordinates")
 
-def formatallcells(nameofspreadsheet,tabname,gc):
-  sh = gc.open(nameofspreadsheet)
-  currentworksheet = sh.get_worksheet(tabname)
-  list_of_lists = currentworksheet.get_all_values()
-  #element_to_check = checkedname
-  #required_count = 4
-  count = sum(sublist.count(element_to_check) for sublist in list_of_lists)
-  if count == 3:
-    print("GREEN")
-    cell_list = worksheet.findall("Joseph Moffatt")
-    worksheet.format(cell_list, {
-        "backgroundColor": {
-            "red": 0.0,
-            "green":1.0,
-            "blue": 0.0
-        },
-        "fontSize": 12,
-        "bold": True
-    })
-  elif count == 2:
-    print("NO GREEN")
-    cell_list = worksheet.findall("Joseph Moffatt")
-    worksheet.format(cell_list, {
-        "backgroundColor": {
-            "red": 0.0,
-            "green":0.0,
-            "blue": 1.0
-        },
-        "fontSize": 12,
-        "bold": True
-    })
-  elif count == 1:
-    print("RED")
-    cell_list = worksheet.findall("Joseph Moffatt")
-    worksheet.format(cell_list, {
-        "backgroundColor": {
-            "red": 1.0,
-            "green":0.0,
-            "blue": 0.0
-        },
-        "fontSize": 12,
-        "bold": True
-    })
-  else:
-    print("Not enough information")
- 
-  print("All done!")
+    #print(cell.row)
+    #print(cell.col)
+        #keyone_row = get_key_by_value(alphabet_mapping,cell.row)
+        #keyone_column = get_key_by_value(alphabet_mapping,cell.col)
+        rangeofcells = A+str(cell.row)+":"+O+str(cell.row)
+        if count_y == 2:
+            currentworksheet.format(rangeofcells,{
+    "backgroundColor": {
+        "red":1.0,
+        "green":0.0,
+        "blue":0.0
+    }
+}   
+    
+)
+        if count_y == 3:
+            currentworksheet.format(rangeofcells,{
+    "backgroundColor": {
+        "red":0.0,
+        "green":0.0,
+        "blue":1.0
+    }
+}   
+    
+)
+        if count_y == 4:
+            currentworksheet.format(rangeofcells,{
+    "backgroundColor": {
+        "red":0.0,
+        "green":1.0,
+        "blue":0.0
+    }
+}   
+    
+)
+            
+def switch(nameofspreadsheet,tabname,gc):
+    nameofspreadsheet = "DHMS 2024-25"
+    tabname = 'Attendance'
+    sh = gc.open(nameofspreadsheet)
+    currentworksheet = sh.worksheet(tabname)
+    list_of_lists = currentworksheet.get_all_values()
+    for i in list_of_lists:
+        print(i)
+    Email = input("Which email would you like to change??")
+    cell = currentworksheet.find(Email)
+    row = cell.row
+    #print(row)
+    column = cell.col
+    #print(column)
+    value = cell.value
+    #print(value)
+    #print(list_of_lists[row-1])
+    currentworksheet.update_cell((row),(column),'1919')
+    print("Done!!!")
